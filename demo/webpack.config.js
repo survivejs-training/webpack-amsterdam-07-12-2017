e5e205a38f86588253b933eca0f8c32db03a4de6
Juho Vepsalainen <bebraw@gmail.com>
2017-12-07 11:29:54 +0100

    chore: Set up webpack-merge

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -1,25 +1,45 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const merge = require("webpack-merge");
+
+const parts = require("./webpack.parts");
 
 const PATHS = {
   app: path.join(__dirname, "app"),
   build: path.join(__dirname, "build"),
 };
 
-module.exports = {
-  // Entries have to resolve to files! They rely on Node
-  // convention by default so if a directory contains *index.js*,
-  // it resolves to that.
-  entry: {
-    app: PATHS.app,
+const commonConfig = merge([
+  {
+    entry: {
+      app: PATHS.app,
+    },
+    output: {
+      path: PATHS.build,
+      filename: "[name].js",
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: "Webpack demo",
+      }),
+    ],
   },
-  output: {
-    path: PATHS.build,
-    filename: "[name].js",
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Webpack demo",
-    }),
-  ],
+]);
+
+const productionConfig = merge([]);
+
+const developmentConfig = merge([
+  parts.devServer({
+    // Customize host/port here if needed
+    host: process.env.HOST,
+    port: process.env.PORT,
+  }),
+]);
+
+module.exports = env => {
+  if (env === "production") {
+    return merge(commonConfig, productionConfig);
+  }
+
+  return merge(commonConfig, developmentConfig);
 };
